d085f7b7291419d7820f16a46b3c1e41e78729d7
sujitsingh08rdm <sujitsingh08rdm@gmail.com>
2023-12-18 09:26:40 +0530

    first commmit

---
+++ script.js
@@ -0,0 +1,47 @@
+let access_key = "LgOfBesFRgB7QbTigBw_nSNIRDRcl8bJId9_gIh9V3M";
+let searchInput = document.getElementById("searchInput");
+let searchBtn = document.getElementById("searchBtn");
+let showData = document.querySelector(".showData");
+let moreBtn = document.getElementById("moreBtn");
+
+let page = 1;
+
+const getData = async (searchValue, pageNo) => {
+  let fetching = await fetch(
+    `https://api.unsplash.com/search/photos?query=${searchValue} &perPage=28&P=${pageNo} age&client_id=${access_key}`
+  );
+  let jsonData = await fetching.json();
+  console.log(jsonData.results);
+
+  if (pageNo == 1) {
+    showData.innerHTML = "";
+  }
+
+  if (searchInput.value == "") {
+    showData.innerHTML = `<h1>Please Search</h1>`;
+  } else {
+    document.querySelector(".loadMore").style.display = "block";
+  }
+
+  jsonData.results.forEach(function (data) {
+    console.log(data.urls.small);
+
+    let div = document.createElement("div");
+    div.classList.add("card");
+    showData.appendChild(div);
+
+    div.innerHTML = `
+        <img src=${data.urls.small} alt="" />
+        <a href="${data.links.html}" target="_blank">${data.alt_description}</a>`;
+  });
+};
+
+searchBtn.addEventListener("click", function () {
+  let searchValue = searchInput.value;
+  getData(searchValue, 1);
+});
+
+moreBtn.addEventListener("click", function () {
+  let searchValue = searchInput.value;
+  getData(searchValue, page++);
+});
